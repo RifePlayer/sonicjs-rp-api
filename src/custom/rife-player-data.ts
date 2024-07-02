b9d6eda0cc0c0e4f2d7ba6aa5b2c718b3771a147
Lane <ldc0618@gmail.com>
2024-07-02 13:41:19 -0700

    just buffer

--- src/custom/rife-player-data.ts
+++ src/custom/rife-player-data.ts
@@ -72,10 +72,10 @@ export async function processStripeWebhook(ctx) {
     // const text = await ctx.req.text()
 
     const bodyArrayBuffer = await ctx.req.arrayBuffer();
-    const bodyRaw = Buffer.from(new Uint8Array(bodyArrayBuffer));
+    // const bodyRaw = Buffer.from(new Uint8Array(bodyArrayBuffer));
 
     event = await stripe.webhooks.constructEventAsync(
-      bodyRaw,
+      bodyArrayBuffer,
       sig,
       stipeSecret
     );
