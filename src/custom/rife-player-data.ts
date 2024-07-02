d696e57499575985e5538a414613b2463a733477
Lane <ldc0618@gmail.com>
2024-07-02 08:53:57 -0700

    text

--- src/custom/rife-player-data.ts
+++ src/custom/rife-player-data.ts
@@ -65,13 +65,13 @@ export async function processStripeWebhook(ctx) {
     // const jsonBody = JSON.parse(body)
 
     // console.log('jsonBody', jsonBody);
-    const raw = await ctx.req.raw.body
+    // const raw = await ctx.req.raw.body
     // const rawFull = await ctx.req.raw;
     // const text = await ctx.req.text()
     // const raw = await ctx.req.parseBody();
-    // const text = await ctx.req.text()
+    const text = await ctx.req.text()
 
-    event = await stripe.webhooks.constructEventAsync(raw, sig, stipeSecret);
+    event = await stripe.webhooks.constructEventAsync(text, sig, stipeSecret);
 
     console.log(event);
   } catch (err) {
